42cdb1b4b3fa09d03be9a7a83db621d8e11b529a
Nishant Shrivastava <nishant.nish05@gmail.com>
2016-11-27 14:56:59 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var middleware = {
+	requireAuthentication: function (request, response, next) {
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function (request, response, next) {
+		var date = new Date().toString();
+		console.log('Request: ' + request.method + ' ' + request.originalUrl + ' on date: ' + date);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+app.get('/about', middleware.requireAuthentication, function (request, response) {
+	response.send('About us.');
+});
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function () {
+	console.log('Express server started on port: ' + PORT);
+});
